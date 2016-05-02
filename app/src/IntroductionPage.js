54382a5585984b30e0b753fb0f45482d0cc10108
Craig M. Wellington <cmwellington@tamu.edu>
2016-05-02 18:06:21 -0500

    wip: start writing introduction

--- app/src/IntroductionPage.js
+++ app/src/IntroductionPage.js
@@ -4,212 +4,52 @@ import CodeExample from './CodeExample';
 import NavMain from './NavMain';
 import PageHeader from './PageHeader';
 import PageFooter from './PageFooter';
+import A from './SmartAnchor';
 
-const IntroductionPage = React.createClass({
-  render() {
-    return (
-      <div>
-        <NavMain activePage="introduction" />
+const IntroductionPage = (props) => {
+  return (
+    <div>
+      <NavMain activePage="introduction" />
 
-        <PageHeader
-          title="Introduction"
-          subTitle="The most popular front-end framework, rebuilt for React."/>
+      <PageHeader
+        title="Introduction"
+        subTitle="What am I reading here?"
+      />
 
-        <div className="container compendium-container">
-          <div className="row">
-            <div className="col-md-9" role="main">
-              <div className="compendium-section">
-                <p className="lead">
-                  React-Bootstrap is a library of reusable front-end components.
-                  You'll get the look-and-feel of Twitter Bootstrap,
-                  but with much cleaner code, via Facebook's React.js framework.
-                </p>
+      <div className="container compendium-container">
+        <div className="row">
+          <div className="col-md-12" role="main">
+            <div className="compendium-section">
+              <h4 className='lead'>
+                Compendium is a neophyte's guide to the Riot Games API for League of Legends.
+              </h4>
+              <p>
+                Although Riot provides a <A href='https://developer.riotgames.com/docs/getting-started'>Getting Started</A> page, many newcomers opine their struggles on the forums when starting out.
+                There's a lot you can do with the Riot API, and a bit you need to know before hand... one page just can't quite cover everything you need to know to get started.
+                Compendium is a step-by-step walkthrough to get you going.
+              </p>
 
-                <p>
-                  Let's say you want a small button that says "Something",
-                  to trigger the function someCallback.
-                  If you were writing a native application,
-                  you might write something like:
-                </p>
+              <h4 className='lead'>
+                Compendium targets the novice programmer.
+              </h4>
+              <p>
+                Perhaps you have taken a course or two on programming?
+                Or perhaps you already know what you want to build, but found the official documentation a bit terse?
+                You want to try your hand at the Riot API challenge, but you're having trouble getting off the ground?
+                This is the guide for you.
+              </p>
+              <p>
+                If you are completely new to programming, even this guide may pose a challenge.
+                However, I would encourge you to try, as every program every written was once just the desire of a would-be programmer, who they themselves had to set forth and learn, just the same.
+              </p>
 
-                <div>
-                  <CodeExample
-                    mode="javascript"
-                    codeText={
-`button(size=SMALL, color=GREEN, text="Something", onClick=someCallback)`
-                    }
-                  />
-                </div>
-
-                <p>
-                  With the most popular web front-end framework,
-                  Twitter Bootstrap, you'd write this in your HTML:
-                </p>
-
-                <div className="highlight">
-                  <CodeExample
-                    mode="htmlmixed"
-                    codeText={
-`<button id="something-btn" type="button" class="btn btn-success btn-sm">
-  Something
-</button>`
-                    }
-                  />
-                </div>
-
-                <p>
-                  And something like
-                  <code className="js">
-                    $('#something-btn').click(someCallback);
-                  </code>
-                  in your Javascript.
-                </p>
-
-                <p>
-                  By web standards this is quite nice,
-                  but it's still quite nasty.
-                  React-Bootstrap lets you write this:
-                </p>
-
-                <div className="highlight">
-                  <CodeExample
-                    mode="javascript"
-                    codeText={
-`<Button bsStyle="success" bsSize="small" onClick={someCallback}>
-  Something
-</Button>`
-                    }
-                  />
-                </div>
-
-                <p>
-                  The HTML/CSS implementation details are abstracted away,
-                  leaving you with an interface that more closely resembles
-                  what you would expect to write in other programming languages.
-                </p>
-
-                <h2>A better Bootstrap API using React.js</h2>
-
-                <p>
-                  The Bootstrap code is so repetitive because HTML and CSS
-                  do not support the abstractions necessary for a nice library
-                  of components. That's why we have to write <code>btn</code>
-                  three times, within an element called <code>button</code>.
-                </p>
-
-                <p>
-                  <strong>
-                    The React.js solution is to write directly in Javascript.
-                  </strong> React takes over the page-rendering entirely.
-                  You just give it a tree of Javascript objects,
-                  and tell it how state is transmitted between them.
-                </p>
-
-                <p>
-                  For instance, we might tell React to render a page displaying
-                  a single button, styled using the handy Bootstrap CSS:
-                </p>
-
-                <div className="highlight">
-                  <CodeExample
-                    mode="javascript"
-                    codeText={
-`var button = React.DOM.button({
-  className: "btn btn-lg btn-success",
-  children: "Register"
-});
-
-React.render(button, mountNode);`
-                    }
-                  />
-                </div>
-
-                <p>
-                  But now that we're in Javascript, we can wrap the HTML/CSS,
-                  and provide a much better API:
-                </p>
-
-                <div className="highlight">
-                  <CodeExample
-                    mode="javascript"
-                    codeText={
-`var button = ReactBootstrap.Button({
-  bsStyle: "success",
-  bsSize: "large",
-  children: "Register"
-});
-
-React.render(button, mountNode);`
-                    }
-                  />
-                </div>
-
-                <p>
-                  React-Bootstrap is a library of such components,
-                  which you can also easily extend and enhance
-                  with your own functionality.
-                </p>
-
-                <h3>JSX Syntax</h3>
-
-                <p>
-                  While each React component is really just a Javascript object,
-                  writing tree-structures that way gets tedious.
-                  React encourages the use of a syntactic-sugar called JSX,
-                   which lets you write the tree in an HTML-like syntax:
-                </p>
-
-                <div className="highlight">
-                  <CodeExample
-                    mode="javascript"
-                    codeText={
-`var buttonGroupInstance = (
-  <ButtonGroup>
-    <DropdownButton bsStyle="success" title="Dropdown">
-      <MenuItem key="1">Dropdown link</MenuItem>
-      <MenuItem key="2">Dropdown link</MenuItem>
-    </DropdownButton>
-    <Button bsStyle="info">Middle</Button>
-    <Button bsStyle="info">Right</Button>
-  </ButtonGroup>
-);
-
-React.render(buttonGroupInstance, mountNode);`
-                    }
-                  />
-                </div>
-
-                <p>
-                  Some people's first impression of React.js is that it seems
-                  messy to mix Javascript and HTML in this way.
-                  However, compare the code required to add
-                  a dropdown button in the example above to the <a
-                  href="http://getbootstrap.com/javascript/#dropdowns">
-                  Bootstrap Javascript</a> and <a
-                  href="http://getbootstrap.com/components/#btn-dropdowns">
-                  Components</a> documentation for creating a dropdown button.
-                  The documentation is split in two because
-                  you have to implement the component in two places
-                  in your code: first you must add the HTML/CSS elements,
-                  and then you must call some Javascript setup
-                  code to wire the component together.
-                </p>
-
-                <p>
-                  The React-Bootstrap component library tries to follow
-                  the React.js philosophy that a single piece of functionality
-                  should be defined in a single place.
-                  View the current React-Bootstrap library on the <a
-                  href="/components.html">components page</a>.
-                </p>
-              </div>
             </div>
           </div>
         </div>
-        <PageFooter />
       </div>
-    );
-  }
-});
+      <PageFooter />
+    </div>
+  );
+};
 
 export default IntroductionPage;
