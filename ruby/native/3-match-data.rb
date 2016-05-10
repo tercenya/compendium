3ac9f0295f172aad6fc1f1dfec8155f412dfca10
Craig M. Wellington <cmwellington@tamu.edu>
2016-05-09 23:13:27 -0500

    remove debugging code

--- ruby/native/3-match-data.rb
+++ ruby/native/3-match-data.rb
@@ -14,7 +14,7 @@ include_timeline = false
 
 # build the URI
 uri = URI("https://na.api.pvp.net/api/lol/na/v2.2/match/#{match_id}?includeTimeline=#{include_timeline}&api_key=#{API_KEY}")
-puts uri.to_s
+
 # call the Riot API
 json = Net::HTTP.get(uri)
 
